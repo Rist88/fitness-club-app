11c9ed0f9a798d20d6e85032b22ddf63b7655195
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-11-17 19:25:07 +0200

    TF-48 Add visualisation for getPrograms in view
    - create basic store
    - add reselect, create selector using it
    - enable CORS on the backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { selectPrograms, store } from './lib/store';
+
+function FitnessClubApp() {
+  const dispatch = useDispatch();
+  const programs = useSelector(selectPrograms);
+
+  useEffect(() => {
+    // console.log(programs);
+    dispatch({ type: 'PROGRAMS_FETCH_SUCCEEDED' });
+  }, []);
+
+  return (
+    <>
+      <div className='App'>
+        {programs?.map(program => (
+          <div key={program.id}>
+            <h4>{program.name}</h4>
+            <p>{program.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
 function App(): JSX.Element {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <FitnessClubApp />
+    </Provider>
   );
 }
 
 export default App;
+
+// default app template
+// import logo from './logo.svg';
+// <div className='App'>
+//   <header className='App-header'>
+//     <img src={logo} className='App-logo' alt='logo' />
+//     <p>
+//       Edit <code>src/App.tsx</code> and save to reload.
+//     </p>
+//     <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+//       Learn React
+//     </a>
+//   </header>
+// </div>
